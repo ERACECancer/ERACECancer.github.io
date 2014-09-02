04c9db4b6119b75a7820394cfd9e9ddb78f1773b
Piper Chester <piperchester@gmail.com>
2014-09-01 19:10:59 -0700

    Fix some links

--- js/app.js
+++ js/app.js
@@ -2,26 +2,6 @@ $(".navbar-fixed-top").addClass('animated fadeInDown');
 
 var eraceApp = angular.module('eraceApp', ['ngRoute']);
 
-//search controller
-// eraceApp.controller('searchController', function($scope, $http, $rootScope) {
-//     apiBaseURL = 'https://ttlc.intuit.com/api/v1/'
-//     requestLoading = false;
-//     $scope.searchButtonClicked = function() {
-//         requestString = apiBaseURL + 'search/posts?q=' + encodeURIComponent($scope.searchValue);
-//         if(!requestLoading) {
-//             $http({method : 'GET', url : requestString})
-//             .success(function(data, status, headers, config) {
-//                 $rootScope.resultData = data;
-//                 window.location = '#/results';
-//             }).
-//
-//             error(function(data, status, headers, config) {
-//
-//             });
-//         }
-//     };
-// });
-
 //peer expert controller
 eraceApp.controller('educationalModulesController', function($scope) {
 
@@ -33,31 +13,6 @@ eraceApp.controller('homeController', function($scope) {
   //   window.location = '#/peer-connection';
   // }, 2000);
 });
-
-//chat controller
-eraceApp.controller('chatController', function($scope) {
-// array questions
-
-var nextIndex = 0;
-var chatInterval = setInterval(function(){
-  if (nextChat[nextIndex] === undefined){
-    document.getElementById("resolutionButton").style.opacity = "1";
-    document.getElementById("cpaButton").style.opacity = "1";
-    clearInterval(chatInterval)
-  } else {
-    document.getElementById(chatIDs[nextIndex]).innerHTML = nextChat[nextIndex];
-    document.getElementById(chatIDs[nextIndex++]).style.opacity = "1";
-  }
-}, 1000);
-
-$scope.resolutionButtonClick = function(){
-  window.location ="#/expert";
-}
-
-$scope.cpaButtonClick = function(){
-  window.location = '#/cpa';
-}});
-
 
 
 //controllers definition
@@ -78,11 +33,15 @@ eraceApp.config(['$routeProvider', function($routeProvider) {
         })
 
         .when('/disparity', {
-            templateUrl : './templates/disparity.htm'
+            templateUrl : './templates/disparity.htm#disparityHome'
         })
 
         .when('/community', {
             templateUrl : './templates/community.htm'
+        })
+
+        .when('/research', {
+            templateUrl : './templates/research.htm'
         })
 
         .otherwise({
