3d42b3234ec853757bd3996ad5fce3a499dbb89e
Piper Chester <piperchester@gmail.com>
2015-03-25 11:03:55 -0400

    Minify CSS + HTML

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,14 @@ var minifyCSS   = require('gulp-minify-css');
 var cssbeautify = require('gulp-cssbeautify');
 var minifyHTML  = require('gulp-minify-html');
 var prettify    = require('gulp-prettify');
-  
+
+// Compress for prod  
 gulp.task('minify-css', function() {
   return gulp.src('./css/*.css')
-    .pipe(minifyCSS({
-      keepBreaks:true
-    }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./css'))
 });
 
-gulp.task('prettify-css', function() {
-    return gulp.src('./dist/*.css')
-        .pipe(cssbeautify())
-        .pipe(gulp.dest('./css/'));;
-});
- 
 gulp.task('minify-html', function() {
   return gulp.src('./index.html')
     .pipe(minifyHTML({
@@ -25,6 +18,13 @@ gulp.task('minify-html', function() {
       spare: true
     }))
     .pipe(gulp.dest('./'));
+});
+
+// Decompress
+gulp.task('prettify-css', function() {
+    return gulp.src('./css/*.css')
+        .pipe(cssbeautify())
+        .pipe(gulp.dest('./css/'));;
 });
 
 gulp.task('prettify-html', function() {
