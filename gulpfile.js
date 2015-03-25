8a80f398dc3a90ca7e45726f52649197f953fdfa
Piper Chester <piperchester@gmail.com>
2015-03-25 11:00:33 -0400

    Add beautify + minify HTML gulp commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var minifyCSS   = require('gulp-minify-css');
 var cssbeautify = require('gulp-cssbeautify');
+var minifyHTML  = require('gulp-minify-html');
+var prettify    = require('gulp-prettify');
   
 gulp.task('minify-css', function() {
   return gulp.src('./css/*.css')
@@ -14,4 +16,21 @@ gulp.task('prettify-css', function() {
     return gulp.src('./dist/*.css')
         .pipe(cssbeautify())
         .pipe(gulp.dest('./css/'));;
+});
+ 
+gulp.task('minify-html', function() {
+  return gulp.src('./index.html')
+    .pipe(minifyHTML({
+      conditionals: true,
+      spare: true
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('prettify-html', function() {
+  gulp.src('index.html')
+    .pipe(prettify({
+      indent_size: 2
+    }))
+    .pipe(gulp.dest('./'))
 });
